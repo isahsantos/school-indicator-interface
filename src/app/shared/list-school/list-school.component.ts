3359af6b2880d9cbf54b996092bc233df48869d6
Isabela Santos <isa2014mgspn@gmail.com>
2024-09-10 10:22:41 -0300

    feat: add estrutura

---
+++ src/app/shared/list-school/list-school.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-list-school',
+  standalone: true,
+  imports: [],
+  templateUrl: './list-school.component.html',
+  styleUrl: './list-school.component.scss'
+})
+export class ListSchoolComponent {
+
+}
